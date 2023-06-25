a3ad78d80d5b72831c9fc4162f140ff948f49ea7
Enz1n <83191216+Enz1n@users.noreply.github.com>
2023-06-25 19:41:38 -0300

    add timememorycounter to filesinfo

--- Obligatorio/src/Main/GetFilesInfo.java
+++ Obligatorio/src/Main/GetFilesInfo.java
@@ -12,21 +12,24 @@ import java.io.IOException;
 
 
 public class GetFilesInfo {
-    private static final String csvFile = "src/main/resources/f1_dataset_test.csv";
+    private static final String csvFile = "src/main/resources/f1_dataset.csv";
     private static final String driversFile = "src/main/resources/drivers.txt";
     public MyLinkedList<String> driversLinkedList = new MyLinkedList<>();
     public  MyLinkedList<User> Users = new MyLinkedList<>();
     public  MyLinkedList<Tweets> Tweets = new MyLinkedList<>();
 
     public void GetDriversInfo() {
-        try (BufferedReader br = new BufferedReader(new FileReader(driversFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                driversLinkedList.add(line.toLowerCase());
+        Main.Calculareficiencia(() -> {
+            System.out.println("Leyendo archivo de conductores...");
+            try (BufferedReader br = new BufferedReader(new FileReader(driversFile))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    driversLinkedList.add(line.toLowerCase());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
     }
     public static String formatDate(String dateString) {
         String[] dateTimeParts = dateString.split(" "); // Dividir la cadena en fecha y hora
@@ -42,53 +45,60 @@ public class GetFilesInfo {
     }
 
     public void GetUsersInfo() throws FileNotValidException {
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
-             CSVParser csvParser = new CSVParser(br, CSVFormat.DEFAULT)) {
-            int count = 0;
-            boolean skipFirstLine = true;
-            for (CSVRecord csvRecord : csvParser) {
-                if (skipFirstLine) {
-                    skipFirstLine = false;  // Cambiar el valor después de leer la primera línea
-                    continue;  // Saltar el procesamiento de la primera línea
-                }
-                count = ++count;
-                MyLinkedList<Hashtag> hashtags = new MyLinkedList<>();
-                String[] values = csvRecord.values();
-                try {
-                    Tweets tweet = new Tweets();
-                    String dateString = values[9];
-                    tweet.setDate(formatDate(dateString));
-                    tweet.setId(Long.valueOf(values[0]));
-                    tweet.setContent(values[10]);
-                    String hashtagsString = values[11];
-                    if (!hashtagsString.isEmpty()) {
-                        String[] hashtagArray = hashtagsString.replace("[", "").replace("]", "").replace("'", "").split(", ");
-                        for (String hashtagText : hashtagArray) {
-                            Hashtag hashtag = new Hashtag();
-                            hashtag.setText(hashtagText);
-                            hashtags.add(hashtag);
+        Main.Calculareficiencia(() -> {
+            System.out.println("Leyendo archivo de usuarios...");
+            try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
+                 CSVParser csvParser = new CSVParser(br, CSVFormat.DEFAULT)) {
+                int count = 0;
+                boolean skipFirstLine = true;
+                for (CSVRecord csvRecord : csvParser) {
+                    if (skipFirstLine) {
+                        skipFirstLine = false;  // Cambiar el valor después de leer la primera línea
+                        continue;  // Saltar el procesamiento de la primera línea
+                    }
+                    count = ++count;
+                    MyLinkedList<Hashtag> hashtags = new MyLinkedList<>();
+                    String[] values = csvRecord.values();
+                    try {
+                        Tweets tweet = new Tweets();
+                        String dateString = values[9];
+                        tweet.setDate(formatDate(dateString));
+                        tweet.setId(Long.valueOf(values[0]));
+                        tweet.setContent(values[10]);
+                        String hashtagsString = values[11];
+                        if (!hashtagsString.isEmpty()) {
+                            String[] hashtagArray = hashtagsString.replace("[", "").replace("]", "").replace("'", "").split(", ");
+                            for (String hashtagText : hashtagArray) {
+                                Hashtag hashtag = new Hashtag();
+                                hashtag.setText(hashtagText);
+                                hashtags.add(hashtag);
+                            }
                         }
-                    }
-                    tweet.setHashtags(hashtags);
-                    tweet.setSource(values[12]);
-                    tweet.setRetweet(Boolean.parseBoolean(values[13]));
-                    Tweets.add(tweet);
-                    User user = new User();
-                    user.setName(values[1]);
-                    user.setFavourites(Integer.parseInt(values[7]));
-                    user.setVerified(Boolean.parseBoolean(values[8]));
-                    if (Users.contains(user)){
-                        User existingUser = Users.getObject(user).getValue();
-                        existingUser.getTweets().add(tweet);
-                    }else {
-                        user.getTweets().add(tweet);
-                        Users.add(user);
-                    }
-                } catch (Exception ignored) {}
+                        tweet.setHashtags(hashtags);
+                        tweet.setSource(values[12]);
+                        tweet.setRetweet(Boolean.parseBoolean(values[13]));
+                        Tweets.add(tweet);
+                        User user = new User();
+                        user.setName(values[1]);
+                        user.setFavourites(Integer.parseInt(values[7]));
+                        user.setVerified(Boolean.parseBoolean(values[8]));
+                        if (Users.contains(user)){
+                            User existingUser = Users.getObject(user).getValue();
+                            existingUser.getTweets().add(tweet);
+                        }else {
+                            user.getTweets().add(tweet);
+                            Users.add(user);
+                        }
+                    } catch (Exception ignored) {}
+                }
+            } catch (IOException e) {
+                try {
+                    throw new FileNotValidException("FILE_ERROR_FORMAT", e);
+                } catch (FileNotValidException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
-        } catch (IOException e) {
-            throw new FileNotValidException("FILE_ERROR_FORMAT", e);
-        }
+        });
     }
 
     public MyLinkedList<String> getDriversLinkedList() {
